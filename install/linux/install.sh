7e40d57d70839f13c4fca593055646c1714bc612
ien646 <1340246+ien646@users.noreply.github.com>
2024-05-19 00:00:00 +0000

    (linux) installer: allow using system libs

--- install/linux/install.sh
+++ install/linux/install.sh
@@ -12,15 +12,23 @@ fi
 read -p "Create application desktop entry? (Y/n):" -n 1 -r REPLY_DESKTOP_ENTRY
 echo ""
 
+read -p "Use system libs? (Y/n):" -n 1 -r REPLY_USE_SYSTEM_LIBS
+echo ""
+
 set -x
 
 sudo rm -rf $TARGET_DIR
 sudo mkdir $TARGET_DIR
 
 sudo cp ./bin $TARGET_DIR/bin -r
-sudo cp ./lib $TARGET_DIR/lib -r
-sudo cp ./plugins $TARGET_DIR/plugins -r
-sudo cp ./rsc $TARGET_DIR/rsc -r
+
+if [[ ! $REPLY_USE_SYSTEM_LIBS ]] then
+    sudo cp ./lib $TARGET_DIR/lib -r
+    sudo cp ./plugins $TARGET_DIR/plugins -r
+    sudo cp ./rsc $TARGET_DIR/rsc -r
+else
+    sudo rm $TARGET_DIR/bin/qt.conf
+fi
 
 if [[ $REPLY_DESKTOP_ENTRY =~ ^[Yy]$ ]] then
     sudo xdg-desktop-menu install igal_qt.desktop --mode system --novendor
