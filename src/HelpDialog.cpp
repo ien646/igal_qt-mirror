71400b4c2eb65e4fb4b22186473b28dadffa9058
ien646 <1340246+ien646@users.noreply.github.com>
2024-05-28 00:00:00 +0000

    improve help dialog formatting

--- src/HelpDialog.cpp
+++ src/HelpDialog.cpp
@@ -5,27 +5,27 @@
 #include "Utils.hpp"
 
 const std::vector<std::string> CONTROLS_LINES = {
-    "<u>Left arrow</u>: Previous item",
-    "<u>Right arrow</u>: Next item",
-    "<u>PgUp</u>: Skip 10 items back",
-    "<u>PgDown</u>: Skip 10 items forward",
-    "<u>Home</u>: Skip to first item",
-    "<u>End</u>: Skip to last item",
-    "<u>O</u>: Open Directory",
-    "<u>Delete</u>: Delete current item",
-    "<u>I</u>: Toggle info overlay",
-    "<u>M</u>: Toggle mute (video)",
-    "<u>Space</u>: Play/Pause (video)",
-    "<u>Num[+]</u>: Zoom in (image/animation)",
-    "<u>Num[-]</u>: Zoom out (image/animation)",
-    "<u>Num[8]</u>: Translate up (image/animation)",
-    "<u>Num[6]</u>: Translate right (image/animation)",
-    "<u>Num[4]</u>: Translate down (image/animation)",
-    "<u>Num[2]</u>: Translate left (image/animation)",
-    "<u>Num[0]</u>: Reset transform (image/animation)",
-    "<u>Ctrl+Shift+Num[+]</u>: Open upscale dialog",
-    "<u>Ctrl+Shift+{Key}</u>: Copy to link directory",
-    "<u>Shift+Return</u>: Open linked directory navigator"
+    "<b>Left arrow</b>: Previous item",
+    "<b>Right arrow</b>: Next item",
+    "<b>PgUp</b>: Skip 10 items back",
+    "<b>PgDown</b>: Skip 10 items forward",
+    "<b>Home</b>: Skip to first item",
+    "<b>End</b>: Skip to last item",
+    "<b>O</b>: Open Directory",
+    "<b>Delete</b>: Delete current item",
+    "<b>I</b>: Toggle info overlay",
+    "<b>M</b>: Toggle mute (video)",
+    "<b>Space</b>: Play/Pause (video)",
+    "<b>Num[+]</b>: Zoom in (image/animation)",
+    "<b>Num[-]</b>: Zoom out (image/animation)",
+    "<b>Num[8]</b>: Translate up (image/animation)",
+    "<b>Num[6]</b>: Translate right (image/animation)",
+    "<b>Num[4]</b>: Translate down (image/animation)",
+    "<b>Num[2]</b>: Translate left (image/animation)",
+    "<b>Num[0]</b>: Reset transform (image/animation)",
+    "<b>Ctrl+Shift+Num[+]</b>: Open upscale dialog",
+    "<b>Ctrl+Shift+{Key}</b>: Copy to link directory",
+    "<b>Shift+Return</b>: Open linked directory navigator"
 };
 
 HelpDialog::HelpDialog(QWidget* parent)
@@ -39,7 +39,8 @@ HelpDialog::HelpDialog(QWidget* parent)
 
     for(size_t i = 0; i < CONTROLS_LINES.size(); ++i)
     {
-        const std::string& line = CONTROLS_LINES[i];
+        size_t index = (i / 2) + ((1 + CONTROLS_LINES.size() / 2) * (i % 2));
+        const std::string& line = CONTROLS_LINES[index];
         const size_t col = i % 2;
         const size_t row = i / 2;
 
